309238b1324f355e47d7df1f52a8955f8615eabf
Parziphal <blackmesa@ubuntu-server.com>
2013-10-02 19:22:18 -0500

    fixed target dir

---
+++ lib/Rails/WillPaginate/Config.php
@@ -0,0 +1,57 @@
+<?php
+namespace WillPaginate;
+
+class Config
+{
+    /**
+     * Sets a default custom renderer class.
+     * A custom renderer class can be created, extending
+     * Renderer\AbstractRenderer.
+     */
+    protected $renderer = 'bootstrap3';
+    
+    /**
+     * Show pagination even if there's only 1 page
+     */
+    protected $alwaysShow = false;
+    
+    /**
+     * Set custom configuration values through set(),
+     * retrieve with get().
+     */
+    protected $custom = [];
+    
+    public function renderer()
+    {
+        return $this->renderer;
+    }
+    
+    public function setRenderer($renderer)
+    {
+        $this->renderer = $renderer;
+    }
+    
+    public function alwaysShow()
+    {
+        return $this->alwaysShow;
+    }
+    
+    public function setAlwaysShow($alwaysShow)
+    {
+        $this->alwaysShow = $alwaysShow;
+    }
+    
+    public function set($key, $value)
+    {
+        $this->custom[$key] = $value;
+    }
+    
+    public function get($key)
+    {
+        if (isset($this->custom[$key])) {
+            return $this->custom[$key];
+        } else {
+            return null;
+        }
+    }
+}
---
+++ lib/Rails/WillPaginate/Renderer/Bootstrap3.php
@@ -0,0 +1,86 @@
+<?php
+namespace WillPaginate\Renderer;
+
+class Bootstrap3 extends Base
+{
+    public function toHtml()
+    {
+        $html = implode(array_map(function($item){
+            if (is_int($item)) {
+                return $this->pageNumber($item);
+            } else {
+                return $this->$item();
+            }
+        }, $this->pagination()), $this->options['link_separator']);
+        
+        return $this->htmlContainer($html);
+    }
+    
+    protected function containerAttributes()
+    {
+        $classes = ['pagination'];
+        
+        if (isset($this->options['size'])) {
+            switch ($this->options['size']) {
+                case 'large':
+                    $classes[] = 'pagination-lg';
+                    break;
+                case 'small':
+                    $classes[] = 'pagination-sm';
+                    break;
+            }
+        }
+        
+        return ['class' => $classes];
+    }
+    
+    protected function htmlContainer($html)
+    {
+        return $this->tag('ul', $html, $this->containerAttributes());
+    }
+
+    protected function defaultOptions()
+    {
+        return array_merge(parent::defaultOptions(), [
+            'previous_label' => '«',
+            'next_label'     => '»'
+        ]);
+    }
+    
+    protected function pageNumber($page)
+    {
+        if ($page != $this->collection->currentPage()) {
+            return $this->tag('li', $this->link($page, $page, ['rel' => $this->relValue($page)]));
+        } else {
+            return $this->tag('li', $this->tag('span', $page), ['class' => 'active']);
+        }
+    }
+    
+    protected function gap()
+    {
+        return $this->tag('li', $this->link('&hellip;', "#"), ['class' => 'disabled']);
+    }
+    
+    protected function previousPage()
+    {
+        $num = $this->collection->currentPage() > 1 ?
+                    $this->collection->currentPage() - 1 : false;
+        return $this->previousOrNextPage($num, $this->options['previous_label'], 'prev');
+    }
+    
+    protected function nextPage()
+    {
+        $num = $this->collection->currentPage() < $this->collection->totalPages() ?
+                    $this->collection->currentPage() + 1 : false;
+        return $this->previousOrNextPage($num, $this->options['next_label'], 'next');
+    }
+    
+    protected function previousOrNextPage($page, $text, $classname)
+    {
+        if ($page) {
+            return $this->tag('li', $this->link($text, $page), ['class' => $classname]);
+        } else {
+            return $this->tag('li', $this->tag('span', $text), ['class' => $classname . ' disabled']);
+        }
+    }
+}
