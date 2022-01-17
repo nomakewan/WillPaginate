d8e1909c14c77acb94287044bae103f1f667c187
nomakewan <nomakewan@users.noreply.github.com>
2022-01-17 14:59:32 -0800

    Update Bootstrap3.php

--- lib/Rails/WillPaginate/Renderer/Bootstrap3.php
+++ lib/Rails/WillPaginate/Renderer/Bootstrap3.php
@@ -5,13 +5,13 @@ class Bootstrap3 extends Base
 {
     public function toHtml()
     {
-        $html = implode(array_map(function($item){
+        $html = implode($this->options['linkSeparator'], array_map(function($item){
             if (is_int($item)) {
                 return $this->pageNumber($item);
             } else {
                 return $this->$item();
             }
-        }, $this->pagination()), $this->options['linkSeparator']);
+        }, $this->pagination()));
         
         return $this->htmlContainer($html);
     }
